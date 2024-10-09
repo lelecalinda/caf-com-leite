a904a9ba65cb8830e7832b11462c6ee86f522f8d
lelecalinda <142261752+lelecalinda@users.noreply.github.com>
2024-10-09 11:43:29 -0300

    gh

--- perguntas.js
+++ perguntas.js
@@ -1,47 +1,65 @@
-Criarcartão (‘matemática','1. O que é uma equação do segundo grau?','É uma conta com \(x^2\), como \(x^2 + 3x + 2 = 0\).')
+Criacartão
+(‘matemática','1. O que é uma equação do segundo grau?','É uma conta com \(x^2\), como \(x^2 + 3x + 2 = 0\).')
 
-criaCartão('Programação','2. O que é um loop em programação?','Um loop é uma estrutura que repete um bloco de código várias vezes até que uma condição seja atendida ou deixe de ser verdadeira.')
+criaCartão
+('Programação','2. O que é um loop em programação?','Um loop é uma estrutura que repete um bloco de código várias vezes até que uma condição seja atendida ou deixe de ser verdadeira.')
 
-criaCartão('química','3. O que é um átomo?','É a menor parte que forma tudo ao nosso redor.')
+criaCartão
+('química','3. O que é um átomo?','É a menor parte que forma tudo ao nosso redor.')
 
-criaCartão('biologia','4. O que é fotossíntese?','É o processo pelo qual as plantas usam a luz do sol para fazer comida.')
+criaCartão
+('biologia','4. O que é fotossíntese?','É o processo pelo qual as plantas usam a luz do sol para fazer comida.')
 
-criaCartão (‘história','5. Quem foi o primeiro presidente do Brasil?','Foi o Marechal Deodoro da Fonseca.')
+criaCartão 
+(‘história','5. Quem foi o primeiro presidente do Brasil?','Foi o Marechal Deodoro da Fonseca.')
 
-criaCartão('português','6. O que é um verbo?','É uma palavra que indica ação, como correr, falar e comer.)
+criaCartão
+('português','6. O que é um verbo?','É uma palavra que indica ação, como correr, falar e comer.)
 
-criaCartão('geografia','7. O que são os continentes','Grandes pedaços de terra como a África e a Ásia.')
+criaCartão
+('geografia','7. O que são os continentes','Grandes pedaços de terra como a África e a Ásia.')
 
-criaCartão('matemática','8. Qual é a fórmula da área de um triângulo?','É a base vezes a altura, dividido por 2.)
+criaCartão
+('matemática','8. Qual é a fórmula da área de um triângulo?','É a base vezes a altura, dividido por 2.)
 
-criaCartão('programação','9. O que é uma variável em programação?','Uma variável é um nome que usamos para guardar um valor, como um número ou uma palavra, no código.')
+criaCartão
+('programação','9. O que é uma variável em programação?','Uma variável é um nome que usamos para guardar um valor, como um número ou uma palavra, no código.')
 
-criaCartão('química','10. O que é um elemento químico?','uma substância básica, como ouro ou oxigênio.')
+criaCartão
+('química','10. O que é um elemento químico?','uma substância básica, como ouro ou oxigênio.')
 
-Criacartão ( 'Matemática',' 11. Matemática-Qual o valor de pi?',
-'3.14')
+Criacartão
+( 'Matemática',' 11. Matemática-Qual o valor de pi?','3.14')
 
 
-Criacartão('História','13.Assumiu o governo, após a saída de Vargas',
+Criacartão
+('História','13.Assumiu o governo, após a saída de Vargas',
 'Dutra')
 
 Criacartão('Biologia','14. Quem foi Mendel?', 'Biólogo e botânico austríaco')
 
-Criacartão('O que é ligação covalente?','15. Uma ligação covalente é a união entre dois átomos que compartilham pares de elétrons para alcançar maior estabilidade.')
+Criacartão
+('O que é ligação covalente?','15. Uma ligação covalente é a união entre dois átomos que compartilham pares de elétrons para alcançar maior estabilidade.')
 
 
-Criacartão('Português','16. O que é uma metáfora?','Uma metáfora é uma comparação implícita entre duas coisas.')
+Criacartão
+('Português','16. O que é uma metáfora?','Uma metáfora é uma comparação implícita entre duas coisas.')
 
 
-Criacartão('programação','17.Qual é a saída do seguinte código em Python? ',' A saída é `[1, 3, 5]`. O fatiamento `x[::2]` seleciona todos os elementos da lista `x` com um passo de 2.')
+Criacartão
+('programação','17.Qual é a saída do seguinte código em Python? ',' A saída é `[1, 3, 5]`. O fatiamento `x[::2]` seleciona todos os elementos da lista `x` com um passo de 2.')
 
-Criacartão('química','18.O que é um ácido?','É uma substância que libera íons hidrogênio (H⁺) em solução aquosa.')
+Criacartão
+('química','18.O que é um ácido?','É uma substância que libera íons hidrogênio (H⁺) em solução aquosa.')
 
-Criacartão('história','19. O que foi a Inconfidência Mineira?','Foi uma tentativa de independência em Minas Gerais no final do século XVIII contra o domínio português.')
-
-
-Criacartão('Geografia','20. O que é um rio de planície?','É um rio que flui por áreas baixas, com um curso largo e menos inclinado.')
-
+Criacartão
+('história','19. O que foi a Inconfidência Mineira?','Foi uma tentativa de independência em Minas Gerais no final do século XVIII contra o domínio português.')
 
 
-Criacartão('Biologia','Qual é a função das mitocôndrias?','Produzir energia (ATP) através da respiração celular.')
+Criacartão
+('Geografia','20. O que é um rio de planície?','É um rio que flui por áreas baixas, com um curso largo e menos inclinado.')
+
+
+
+Criacartão
+('Biologia','Qual é a função das mitocôndrias?','Produzir energia (ATP) através da respiração celular.')
