f4a528dbf00c6f289a9049e41bcf3565407e090c
lelecalinda <142261752+lelecalinda@users.noreply.github.com>
2024-10-16 11:18:19 -0300

    n

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria,pergunta,resposerta){
+function criaCartao(categoria,pergunta,resposta){
    let container = document.getElementById('container');
    let cartao = document.createElement('article');
    cartao.className = 'café com leite';
