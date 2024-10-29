4f9b46e3a1546ee14d847073c4310c3431c586da
lelecalinda <142261752+lelecalinda@users.noreply.github.com>
2024-10-29 08:25:17 -0300

    j

--- projeto2.js
+++ projeto2.js
@@ -18,8 +18,8 @@ function criaCartao(categoria,pergunta,resposta){
 
 
  function mostraResposta (card){
-    const resposta = document.querySelector('.resposta-cartao');
-    if(resposta.style.display === 'none') {
+    const resposta = card.querySelector('.resposta-cartao');
+    if (resposta.style.display === 'none') {
         resposta.style.display = 'block'
 
     }
